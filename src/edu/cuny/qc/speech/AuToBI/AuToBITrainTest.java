386cb02d873f77e754ec97e03559bf701fd6f1f4
Andrew Rosenberg <andrew@cs.qc.cuny.edu>
2012-08-08 11:10:55 -0400

    Modify AuToBITrainTest to use the same mechanics as AuToBITrainer.

--- src/edu/cuny/qc/speech/AuToBI/AuToBITrainTest.java
+++ src/edu/cuny/qc/speech/AuToBI/AuToBITrainTest.java
@@ -1,16 +1,11 @@
 package edu.cuny.qc.speech.AuToBI;
 
-import edu.cuny.qc.speech.AuToBI.classifier.WekaClassifier;
 import edu.cuny.qc.speech.AuToBI.core.*;
 import edu.cuny.qc.speech.AuToBI.featureextractor.FeatureExtractorException;
-import edu.cuny.qc.speech.AuToBI.featureset.*;
 import edu.cuny.qc.speech.AuToBI.io.*;
 import edu.cuny.qc.speech.AuToBI.util.AuToBIReaderUtils;
 import edu.cuny.qc.speech.AuToBI.util.AuToBIUtils;
 import edu.cuny.qc.speech.AuToBI.util.ClassifierUtils;
-import weka.classifiers.functions.Logistic;
-
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -27,77 +22,67 @@ public class AuToBITrainTest {
     try {
       List<FormattedFile> training_files = AuToBIReaderUtils.globFormattedFiles(autobi.getParameter("training_files"));
       List<FormattedFile> testing_files = AuToBIReaderUtils.globFormattedFiles(autobi.getParameter("testing_files"));
-      String task = autobi.getParameter("task");
-      String model_file = autobi.getParameter("model_file");
 
       // Added here to comply to comply with the new feature registration changes to FeatureSetPropagator
       autobi.registerAllFeatureExtractors();
       autobi.registerNullFeatureExtractor("speaker_id");
 
-      // implement getFeatureSet() so it returns an appropriate feature
-      // set.
-      FeatureSet training_fs = getFeatureSet(task);
-      autobi.propagateFeatureSet(training_files, training_fs);
+      HashMap<String, AuToBITask> tasks = AuToBIUtils.createTaskListFromParameters(autobi.getParameters());
 
-      FeatureSet testing_fs = getFeatureSet(task);
-      autobi.propagateFeatureSet(testing_files, testing_fs);
+      for (String task_label : tasks.keySet()) {
+        AuToBITask task = tasks.get(task_label);
+        AuToBITrainer trainer = new AuToBITrainer(autobi);
+        try {
 
-      // any other classifier can be slotted in here.
-      WekaClassifier classifier = new WekaClassifier(new Logistic());
-      classifier.train(training_fs);
+          // Tone classification tasks ignore those points that do not have any associated prosodic event
+          if (task_label.equals("phrase_accent_classifier")) {
+            autobi.getParameters().setParameter("attribute_omit", "nominal_PitchAccentType:NOTONE");
+          } else if (task_label.equals("phrase_accent_boundary_tone_classifier")) {
+            autobi.getParameters().setParameter("attribute_omit", "nominal_PhraseAccentBoundaryTone:NOTONE");
+          } else if (task_label.equals("phrase_accent_classifier")) {
+            autobi.getParameters().setParameter("attribute_omit", "nominal_PhraseAccent:NOTONE");
+          } else {
+            autobi.getParameters().setParameter("attribute_omit", "");
+          }
+          trainer.trainClassifier(training_files, task.getFeatureSet(), task.getClassifier());
 
-      // writing model file
-      AuToBIUtils.log("writing model to: " + model_file);
-      FileOutputStream fos;
-      ObjectOutputStream out;
-      try {
-        fos = new FileOutputStream(model_file);
-        out = new ObjectOutputStream(fos);
-        out.writeObject(classifier);
-        out.close();
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
+        } catch (Exception e) {
+          AuToBIUtils.error("Error training classifier for " + task_label);
+          continue;
+        }
+        FeatureSet testing_fs = task.getFeatureSet().newInstance();
+        testing_fs.getDataPoints().clear();
+        autobi.propagateFeatureSet(testing_files, testing_fs);
+
+        // writing model file
+        AuToBIUtils.log("writing model to: " + autobi.getParameter(task_label));
+        FileOutputStream fos;
+        ObjectOutputStream out;
+        try {
+          fos = new FileOutputStream(autobi.getParameter(task_label));
+          out = new ObjectOutputStream(fos);
+          out.writeObject(task.getClassifier());
+          out.close();
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+        // prediction on test set
+        ClassifierUtils.generatePredictions(task.getClassifier(), "hyp", "DEFAULT", testing_fs);
+
+        EvaluationResults er =
+            ClassifierUtils.generateEvaluationResults("hyp", testing_fs.getClassAttribute(), testing_fs);
+
+        EvaluationSummary es = new EvaluationSummary(er);
+
+        AuToBIUtils.log("Test Results on test set\n" + es.toString());
       }
-
-      // prediction on test set
-      ClassifierUtils.generatePredictions(classifier, "hyp", "DEFAULT", testing_fs);
-
-      EvaluationResults er =
-          ClassifierUtils.generateEvaluationResults("hyp", testing_fs.getClassAttribute(), testing_fs);
-
-      EvaluationSummary es = new EvaluationSummary(er);
-
-      AuToBIUtils.log("Test Results on test set\n" + es.toString());
     } catch (FeatureExtractorException e) {
-      e.printStackTrace();
-    } catch (AuToBIException e) {
-      e.printStackTrace();
-    } catch (UnsupportedAudioFileException e) {
       e.printStackTrace();
     } catch (Exception e) {
       e.printStackTrace();
     }
-  }
-
-  /**
-   * @return
-   * @throws AuToBIException
-   */
-  private static FeatureSet getFeatureSet(String task) throws AuToBIException {
-    if (task.equals("pitch_accent_detection"))
-      return new PitchAccentDetectionFeatureSet();
-    if (task.equals("pitch_accent_classification"))
-      return new PitchAccentClassificationFeatureSet();
-    if (task.equals("intonational_phrase_boundary_detection"))
-      return new IntonationalPhraseBoundaryDetectionFeatureSet();
-    if (task.equals("intermediate_phrase_boundary_detection"))
-      return new IntermediatePhraseBoundaryDetectionFeatureSet();
-    if (task.equals("boundary_tone_classification"))
-      return new PhraseAccentBoundaryToneClassificationFeatureSet();
-    if (task.equals("phrase_accent_classification"))
-      return new PhraseAccentClassificationFeatureSet();
-    throw new AuToBIException("No defined feature set for task: " + task);
   }
 }
