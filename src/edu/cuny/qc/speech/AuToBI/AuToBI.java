3578927033fbbcd55cb7134f511a3b550b8f3504
Andrew <andrew@cs.qc.cuny.edu>
2014-05-30 12:59:44 -0400

    fixed a line that shouldnt' have been commented out.

--- src/edu/cuny/qc/speech/AuToBI/AuToBI.java
+++ src/edu/cuny/qc/speech/AuToBI/AuToBI.java
@@ -1400,7 +1400,7 @@ public class AuToBI {
       }
 
       if (hasParameter("out_file")) {
-//        AuToBIUtils.mergeAuToBIHypotheses(this, words);
+        AuToBIUtils.mergeAuToBIHypotheses(this, words);
         String hypothesis_file = getParameter("out_file");
         AuToBIUtils.info("Writing hypotheses to " + hypothesis_file);
         writeTextGrid(words, hypothesis_file);
